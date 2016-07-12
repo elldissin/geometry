616474d3ca1a8bf5264a8cdc35d0bd784735f9b5
elldissin <elldissin@gmail.com>
2016-07-13 00:24:23 +0300

    Worked on thread for listening to server in ServerCommunicator

--- Geometry/src/nubiki/networking/ServerCommunicator.java
+++ Geometry/src/nubiki/networking/ServerCommunicator.java
@@ -22,7 +22,13 @@ public class ServerCommunicator {
 	}
 
 	public void sendEvent(KeyEvent event) {
-		KeyEvent eventFromServer;
+		if (out!=null)
+			try {
+				out.writeObject(event);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 
 	public void closeConnection() {
@@ -44,36 +50,29 @@ public class ServerCommunicator {
 			in = new ObjectInputStream(mySocket.getInputStream());
 			System.out.println("Connection with server established");
 
-			//This part is continously waiting and reading for user input
-			//		Thread inputThread = new Thread (new Runnable(){
-			//			@Override
-			//			public void run() {
-			//				try {
-			//					String fromUser="";
-			//					while ((fromUser = stdIn.readLine()) != null)
-			//						if (fromUser != null)
-			//							out.writeObject(new KeyEvent(fromUser));
-			//				} catch (IOException e) {
-			//					// TODO Auto-generated catch block
-			//					e.printStackTrace();
-			//				}
-			//			}});
-			//		inputThread.start();
-			KeyEvent fromServer;
-			while ((fromServer = (KeyEvent)in.readObject()) != null) {
-				//			System.out.println(fromServer.getMessage());
-				if (fromServer.equals("Server is stopping")) {
-					break;
-				}
-			}
+			//This part is continously waiting for events from server
+			Thread inputThread = new Thread (new Runnable(){
+				@Override
+				public void run() {
+					try {
+						KeyEvent fromServer;
+						System.out.println("Server listening thread started on client");
+						while ((fromServer = (KeyEvent)in.readObject()) != null) {
+							if (fromServer.equals("Server is stopping")) { //fix to exit properly
+								break;
+							}
+						}
+					} catch (IOException | ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}});
+			inputThread.start();
 		}
 		catch (IOException e ) {
 			System.out.println("Couldn't get I/O for the connection to " +
 					hostName);
 			System.exit(1);
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
 		}
 	}
 }

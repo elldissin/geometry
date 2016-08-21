520c1cd5c46dd50329510f28c13e4ef0b7e83320
elldissin <elldissin@gmail.com>
2016-08-21 09:54:48 +0300

    Only PlayerInput is sent to server now, NetworkMessage received;

--- Geometry/src/my/games/geometry/game/objects/Controller.java
+++ Geometry/src/my/games/geometry/game/objects/Controller.java
@@ -10,19 +10,11 @@ import java.util.Set;
 
 import javax.swing.Timer;
 
-import geometry.networking.events.GameEvent;
-import geometry.networking.events.MoveEvent;
-import geometry.networking.events.ShootEvent;
-import geometry.networking.events.StopEvent;
-import geometry.networking.events.TurnEventCCW;
-import geometry.networking.events.TurnEventCW;
+import geometry.networking.PlayerInput;
 import my.games.geometry.events.EventManager;
 import my.games.geometry.networking.ServerCommunicator;
-import geometry.networking.NetworkMessage;
 
 public class Controller implements KeyListener {
-
-	// ServerCommunicator comm;
 	EventManager manager;
 	ServerCommunicator comm;
 	private final Set<Integer> pressed = new HashSet<Integer>();
@@ -30,69 +22,20 @@ public class Controller implements KeyListener {
 
 	public Controller(EventManager manager, final ServerCommunicator comm) {
 		controlledArray = new ArrayList<Controllable>();
-		// comm = new ServerCommunicator();
-		// comm.openConnectionTo("localhost");
 		this.manager = manager;
 		this.comm = comm;
-		//Check every 100ms if there's keys pressed
-		//(This is the Swing Timer they talk about)
-		new Timer(20, new ActionListener(){
+		// Check every 100ms if there's keys pressed
+		// (This is the Swing Timer they talk about)
+		new Timer(20, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				String keysString = "";
-				if(!pressed.isEmpty()){
-					for(Integer pressedCode: pressed) {
-						if (pressedCode == KeyEvent.VK_W) {
-							NetworkMessage msg=new NetworkMessage(pressedCode);
-							GameEvent ev = new MoveEvent(0);
-							msg.setEvent(ev);
-							comm.sendMessage(msg);
-						}
-						if (pressedCode == KeyEvent.VK_D) {
-							NetworkMessage msg=new NetworkMessage(pressedCode);
-							GameEvent ev = new TurnEventCW(0);
-							msg.setEvent(ev);
-							comm.sendMessage(msg);
-						}
-						if (pressedCode == KeyEvent.VK_A) {
-							NetworkMessage msg=new NetworkMessage(pressedCode);
-							GameEvent ev = new TurnEventCCW(0);
-							msg.setEvent(ev);
-							comm.sendMessage(msg);
-						}
-						if (pressedCode == KeyEvent.VK_Q) {
-							NetworkMessage msg=new NetworkMessage(pressedCode);
-							GameEvent ev = new ShootEvent(0);
-							msg.setEvent(ev);
-							comm.sendMessage(msg);
-						}
-
-						if (pressedCode == KeyEvent.VK_UP) {
-							NetworkMessage msg=new NetworkMessage(pressedCode);
-							GameEvent ev = new MoveEvent(1);
-							msg.setEvent(ev);
-							comm.sendMessage(msg);
-						}
-						if (pressedCode == KeyEvent.VK_RIGHT) {
-							NetworkMessage msg=new NetworkMessage(pressedCode);
-							GameEvent ev = new TurnEventCW(1);
-							msg.setEvent(ev);
-							comm.sendMessage(msg);
-						}
-						if (pressedCode == KeyEvent.VK_LEFT) {
-							NetworkMessage msg=new NetworkMessage(pressedCode);
-							GameEvent ev = new TurnEventCCW(1);
-							msg.setEvent(ev);
-							comm.sendMessage(msg);
-						}
-						if (pressedCode == KeyEvent.VK_CONTROL) {
-							NetworkMessage msg=new NetworkMessage(pressedCode);
-							GameEvent ev = new ShootEvent(1);
-							msg.setEvent(ev);
-							comm.sendMessage(msg);
-						}
+				if (!pressed.isEmpty()) {
+					for (Integer pressedCode : pressed) {
+						// playerID 1 is a stub here
+						comm.sendInput(new PlayerInput(1, pressedCode));
 					}
-				} 
+				}
 			}
 		}).start();
 	}
--- Geometry/src/my/games/geometry/networking/ServerCommunicator.java
+++ Geometry/src/my/games/geometry/networking/ServerCommunicator.java
@@ -1,52 +1,59 @@
 package my.games.geometry.networking;
 
-import java.awt.event.KeyEvent;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+
 import geometry.networking.NetworkMessage;
+import geometry.networking.PlayerInput;
+
 public class ServerCommunicator {
 
 	private String hostName;
-	private int portNumber=4444;
+	private int portNumber = 4444;
 	private Socket mySocket;
 	private ObjectOutputStream out;
 	private ObjectInputStream in;
 	private Queue<NetworkMessage> eventsQueue;
-	
+
 	public ServerCommunicator() {
 		eventsQueue = new LinkedList<NetworkMessage>();
 	}
 
 	public void sendMessage(NetworkMessage event) {
-		if (out!=null)
+		if (out != null)
 			try {
 				out.writeObject(event);
-//				System.out.println("Event was sent to server");
+				// System.out.println("Event was sent to server");
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	}
-	
+
+	public void sendInput(PlayerInput input) {
+		if (out != null)
+			try {
+				out.writeObject(input);
+				// System.out.println("Event was sent to server");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+
 	public NetworkMessage getNextMessage() {
 		return eventsQueue.poll();
 	}
 
 	public void closeConnection() {
 		try {
-			if(out!=null)
+			if (out != null)
 				out.close();
-			if(in!=null)
+			if (in != null)
 				in.close();
-			if(mySocket!=null)
+			if (mySocket != null)
 				mySocket.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -62,27 +69,26 @@ public class ServerCommunicator {
 			in = new ObjectInputStream(mySocket.getInputStream());
 			System.out.println("Connection with server established");
 
-			//This part is continously waiting for events from server
-			Thread inputThread = new Thread (new Runnable(){
+			// This part is continously waiting for events from server
+			Thread inputThread = new Thread(new Runnable() {
 				@Override
 				public void run() {
 					try {
 						NetworkMessage fromServer;
 						System.out.println("Server listening thread started on client");
-						while ((fromServer = (NetworkMessage)in.readObject()) != null) {
+						while ((fromServer = (NetworkMessage) in.readObject()) != null) {
 							eventsQueue.add(fromServer);
-							//add something to exit properly							
+							// add something to exit properly
 						}
 					} catch (IOException | ClassNotFoundException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}});
+				}
+			});
 			inputThread.start();
-		}
-		catch (IOException e ) {
-			System.out.println("Couldn't get I/O for the connection to " +
-					hostName);
+		} catch (IOException e) {
+			System.out.println("Couldn't get I/O for the connection to " + hostName);
 			System.exit(1);
 		}
 	}

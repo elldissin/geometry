c544e1e4caec1a474e6cb92b20c4415300f64856
elldissin <elldissin@gmail.com>
2016-08-26 17:23:00 +0300

    Changed camera lock from 0 and 1 to 1 and 2

--- Geometry/src/my/games/geometry/game/engine/ClientRenderEngine.java
+++ Geometry/src/my/games/geometry/game/engine/ClientRenderEngine.java
@@ -1,11 +1,9 @@
 package my.games.geometry.game.engine;
 
 import java.awt.Point;
-import java.util.List;
 
 import my.games.geometry.game.GameCamera;
 import my.games.geometry.game.World;
-import my.games.geometry.game.objects.GameObject;
 
 public class ClientRenderEngine implements RenderEngine {
 	private World world;
@@ -20,14 +18,14 @@ public class ClientRenderEngine implements RenderEngine {
 	@Override
 	public void render() {
 		// ties camera 1 to player 1
-		Point p = new Point((int) world.getObjectByID(0).getPos().x - camera1.getViewWidth() / 2,
-				(int) world.getObjectByID(0).getPos().y - camera1.getViewHeight() / 2);
+		Point p = new Point((int) world.getObjectByID(1).getPos().x - camera1.getViewWidth() / 2,
+				(int) world.getObjectByID(1).getPos().y - camera1.getViewHeight() / 2);
 		camera1.setViewOffset(p);
 		camera1.show(world.getDrawableObjectList());
 
 		// ties camera 2 to player 2
-		p = new Point((int) world.getObjectByID(1).getPos().x - camera2.getViewWidth() / 2,
-				(int) world.getObjectByID(1).getPos().y - camera2.getViewHeight() / 2);
+		p = new Point((int) world.getObjectByID(2).getPos().x - camera2.getViewWidth() / 2,
+				(int) world.getObjectByID(2).getPos().y - camera2.getViewHeight() / 2);
 		camera2.setViewOffset(p);
 		camera2.show(world.getDrawableObjectList());
 	}

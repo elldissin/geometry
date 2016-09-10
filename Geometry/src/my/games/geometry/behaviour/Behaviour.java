200ac13bd3c2451922521e1b67ac6c9523e5e5a4
qwerty367 <Daenisvolkov@gmail.com>
2016-09-11 00:07:18 +0300

    test

--- Geometry/src/my/games/geometry/behaviour/Behaviour.java
+++ Geometry/src/my/games/geometry/behaviour/Behaviour.java
@@ -4,17 +4,25 @@ import my.games.geometry.game.objects.GameObject;
 
 public interface Behaviour {
 	public boolean isSlowable();
-	public boolean isVulnerable();
-	public boolean isBumping();
-	public boolean destructibleOnBump();
-	
-	public void slowDown(GameObject obj, int amount);
-	public void doDamage(GameObject obj, int amount);
-	public void bump(GameObject obj, int amount);
-	
-	public void setSlowable(boolean value);
-	public void setVulnerable(boolean value);
-	public void setBumping(boolean value);
-	public void setDestructibleOnBump(boolean value);
 
+	public boolean isVulnerable();
+
+	public boolean isBumping();
+
+	public boolean destructibleOnBump();
+
+	public void slowDown(GameObject obj, int amount);
+
+	public void doDamage(GameObject obj, int amount);
+
+	public void bump(GameObject obj, int amount);
+
+	public void setSlowable(boolean value);
+
+	public void setVulnerable(boolean value);
+
+	public void setBumping(boolean value);
+
+	public void setDestructibleOnBump(boolean value);
+	// test
 }

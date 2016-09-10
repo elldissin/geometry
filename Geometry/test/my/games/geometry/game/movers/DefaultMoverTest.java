afbfa2af47b58c126c697a82f821609e25a75f67
qwerty367 <Daenisvolkov@gmail.com>
2016-09-10 15:45:36 +0300

    Added DefoultMoverTest

---
+++ Geometry/test/my/games/geometry/game/movers/DefaultMoverTest.java
@@ -0,0 +1,48 @@
+package my.games.geometry.game.movers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import my.games.geometry.game.World;
+import my.games.geometry.game.objects.GameObject;
+
+public class DefaultMoverTest {
+
+	@Test
+	public void gameObjectMovetest() {
+		// arrange
+		World worldTest = new World();
+		GameObject playerTest = worldTest.createGameObject("player", 0, 0, 0.0);
+		// act
+		playerTest.getMover().move(playerTest);
+		// assert
+		assertEquals(playerTest.getPos().x, 5);
+		assertEquals(playerTest.getPos().y, 0);
+	}
+
+	@Test
+	public void gameObjectMoveAngle90test() {
+		// arrange
+		World worldTest = new World();
+		GameObject playerTest = worldTest.createGameObject("player", 0, 0, Math.toRadians(90));
+		// act
+		playerTest.getMover().move(playerTest);
+		// assert
+		assertEquals(playerTest.getPos().x, 0);
+		assertEquals(playerTest.getPos().y, 5);
+	}
+
+	@Test
+	public void gameObjectTurntest() {
+		// arrange
+		World worldTest = new World();
+		GameObject playerTest = worldTest.createGameObject("player", 0, 0, 0.0);
+		// act
+		playerTest.getMover().turn(playerTest, -1);
+		// assert
+		assertEquals(playerTest.getAngle(), -0.1, 0.001);
+
+	}
+
+}

5f0909efc5f7cddbdd80d327fcdfeecdf41f40b6
qwerty367 <Daenisvolkov@gmail.com>
2016-09-11 16:51:32 +0300

    Added  ServerLogService abd SerberEventNotifire but not working

--- Geometry/src/my/games/geometry/game/Server.java
+++ Geometry/src/my/games/geometry/game/Server.java
@@ -103,4 +103,8 @@ public class Server {
 			}
 		}
 	}
+
+	public World getWorld() {
+		return world;
+	}
 }
---
+++ Geometry/src/my/games/geometry/networking/ServerLogService.java
@@ -0,0 +1,46 @@
+package my.games.geometry.networking;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ServerLogService implements Runnable {
+
+	private List<ConnectedClient> clientList;
+	private boolean isRunning = false;
+
+	public ServerLogService() {
+		clientList = Collections.synchronizedList(new ArrayList<ConnectedClient>());
+	}
+
+	@Override
+	public void run() {
+		Socket newConnect;
+		ConnectionWaiter waiter = new ConnectionWaiter();
+		while (true) {
+			if ((newConnect = waiter.acceptConnection()) != null) {
+				ConnectedClient client = new ConnectedClient(newConnect);
+				clientList.add(client);
+			}
+			try {
+				Thread.sleep(5); // To reduce CPU load
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void start() {
+		new Thread(this).start();
+	}
+
+	public List<ConnectedClient> getClientList() {
+		return clientList;
+	}
+
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+}

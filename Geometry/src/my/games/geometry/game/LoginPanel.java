11a1d08271604268bc7f702eb1925751e967af7d
elldissin <elldissin@gmail.com>
2016-09-27 19:52:28 +0300

    Some changes in class layout

--- Geometry/src/my/games/geometry/game/LoginPanel.java
+++ Geometry/src/my/games/geometry/game/LoginPanel.java
@@ -3,52 +3,43 @@ package my.games.geometry.game;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
 
 public class LoginPanel extends JPanel {
-	private List<JLabel> mLabelList;
-	private List<JTextPane> mTextList;
-	private volatile boolean logButtClicked;
 
 	private Client client;
-	private int viewWidth;
-	private int viewHeight;
 	private ClientWindow clientwindow;
+	private JLabel loginLabel, passwordLabel;
+	public JTextField loginField, passwordField;
 
 	LoginPanel(Client client, ClientWindow clientwindow) {
 		this.clientwindow = clientwindow;
 		this.client = client;
-		mLabelList = new ArrayList<JLabel>();
-		mTextList = new ArrayList<JTextPane>();
-		logButtClicked = false;
 
 		this.setLayout(new GridLayout(3, 2, 0, 5));
 
-		JLabel anotherLabel = new JLabel();
-		anotherLabel.setText("login");
-		anotherLabel.setPreferredSize(new Dimension(70, 30));
-		this.add(anotherLabel);
-		JTextField loginField = new JTextField();
+		loginLabel = new JLabel("Login(numeric):");
+		loginLabel.setPreferredSize(new Dimension(70, 30));
+		this.add(loginLabel);
+
+		loginField = new JTextField();
 		loginField.setPreferredSize(new Dimension(70, 30));
 		this.add(loginField);
 
-		JLabel anotherLabel1 = new JLabel();
-		anotherLabel1.setText("Password");
-		anotherLabel1.setPreferredSize(new Dimension(70, 30));
-		this.add(anotherLabel1);
-		JTextField passwordField = new JTextField();
+		passwordLabel = new JLabel("Password:");
+		passwordLabel.setPreferredSize(new Dimension(70, 30));
+		this.add(passwordLabel);
+
+		passwordField = new JTextField();
 		passwordField.setPreferredSize(new Dimension(70, 30));
 		this.add(passwordField);
 
 		JButton loginButt = new JButton();
-		LoginButtonListener loginButtonListener = new LoginButtonListener(client, clientwindow);
+		LoginButtonListener loginButtonListener = new LoginButtonListener(client, clientwindow, this);
 		loginButt.setText("Log in");
 		loginButt.addActionListener(loginButtonListener);
 		loginButt.setPreferredSize(new Dimension(30, 30));

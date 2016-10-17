1eea56acda9fcfc68ae264346301ec849028845f
elldissin <elldissin@gmail.com>
2016-10-17 21:54:12 +0300

    more work on Animator

--- Geometry/src/my/games/geometry/game/renderers/Animator.java
+++ Geometry/src/my/games/geometry/game/renderers/Animator.java
@@ -1,10 +1,16 @@
 package my.games.geometry.game.renderers;
 
 import java.awt.Point;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
-public class Animator {
-	private ArrayList<Point> pointsForCurrentFrame;
+public class Animator implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Point> pointsForCurrentFrame;
 	private int frameCounter;
 
 	public Animator() {
@@ -12,13 +18,15 @@ public class Animator {
 		pointsForCurrentFrame = new ArrayList<Point>();
 	}
 
-	public ArrayList<Point> animatePoints(ArrayList<Point> initialPoints) {
+	public List<Point> animatePoints(List<Point> initialPoints) {
+		pointsForCurrentFrame.clear();
 		frameCounter++;
 		if (frameCounter > 10)
 			frameCounter = 0;
 		for (int i = 0; i < initialPoints.size(); i++) {
-			Point p = initialPoints.get(i);
-			// FIXME add some changing code to points
+			Point p = (Point) initialPoints.get(i).clone();
+			p.setLocation(p.getX() * 0.5, p.getY() * 0.5);
+			pointsForCurrentFrame.add(p);
 		}
 		return pointsForCurrentFrame;
 	}

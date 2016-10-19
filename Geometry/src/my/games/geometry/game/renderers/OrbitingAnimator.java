a2bcb92415fbbd4bb271905f7cc1f735f16ea226
elldissin <elldissin@gmail.com>
2016-10-19 18:59:56 +0300

    OrbitingAnimator added but buggy

---
+++ Geometry/src/my/games/geometry/game/renderers/OrbitingAnimator.java
@@ -0,0 +1,25 @@
+package my.games.geometry.game.renderers;
+
+import java.awt.Point;
+import java.util.List;
+
+public class OrbitingAnimator extends Animator {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void modifyPoints(List<Point> initialPoints, Point center) {
+		double angle = Math.toRadians(frameCounter) * 2;
+		for (int i = 0; i < initialPoints.size(); i++) {
+			Point p = (Point) initialPoints.get(i).clone();
+			pointsForCurrentFrame.add(p);
+		}
+		Point rotatingParticle = new Point(center);
+		rotatingParticle.setLocation(rotatingParticle.getX() + 40, rotatingParticle.getY() + 40);
+		RotateTransform.transform(rotatingParticle, center, angle);
+		pointsForCurrentFrame.add(rotatingParticle);
+	}
+
+}
